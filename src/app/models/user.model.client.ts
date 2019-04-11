16add7870594062006e292c2d81fa6d3f4fb7f7e
shufanxing2017 <xing.sh@husky.neu.edu>
2019-04-11 16:18:17 -0700

    add model

---
+++ src/app/models/user.model.client.ts
@@ -0,0 +1,27 @@
+export class User {
+    _id: String;
+    username: string;
+    password: string;
+
+    userType: string;
+    firstName: string;
+    lastName: string;
+    email: string;
+    restaurantName: string;
+    adsFee: number;
+    employeePermit: string;
+    destription: string;
+
+
+
+
+    constructor( _id, username, password, firstName = 'alice' , lastName = 'chase', email = 'alice@gmail') {
+        this._id = _id;
+        this.username = username;
+        this.password = password;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
+
